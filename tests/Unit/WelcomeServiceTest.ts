1175f2a6c7ea9d64f7979f226cedc1501af76f98
João Lenon <lenonSec7@gmail.com>
2023-01-12 22:55:58 -0300

    chore: update dependencies

--- tests/Unit/WelcomeServiceTest.ts
+++ tests/Unit/WelcomeServiceTest.ts
@@ -1,13 +1,7 @@
-import { Path } from '@athenna/common'
-import { Config } from '@athenna/config'
 import { Test, UnitTestContext } from '@athenna/test'
 import { WelcomeService } from '#app/Services/WelcomeService'
 
 export class WelcomeServiceTest extends Test {
-  async beforeAll() {
-    await Config.loadAll(Path.config())
-  }
-
   async shouldReturnConcreteWelcomePayloadFromService({ assert }: UnitTestContext) {
     const welcomeService = new WelcomeService()
 
